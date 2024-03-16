d11b64126f833e644c801e5a51d4d9af01db1e17
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-03-16 09:30:25 -0300

    Editing the Task Manager Class

--- to_do_list/root/src/app/task_manager.py
+++ to_do_list/root/src/app/task_manager.py
@@ -1,4 +1,5 @@
 from task import Task
+from validate import Validate
 
 
 class Task_manager():
@@ -10,13 +11,22 @@ class Task_manager():
         self.task_list.append(task)
 
     def remove_task(self, index):
+        if not Validate.validate_index(self.task_list, index):
+            raise IndexError("Error - Invalid Index.")
+
         del self.task_list.pop(index)
+    
+    def alter_status(self, status, index):
+        if not Validate.validate_index(self.task_list, index):
+            raise IndexError("Error - Invalid Index.")
+
+        self.task_list[index].status = status
 
     def __str__(self):
-        ...
-
-    def __rep__(self):
-        ...
+        if not self.task_list:
+            return "No tasks found. The task list is currently empty."
+        
+        return "\n".join(str(task) for task in self.task_list)
 
 
 if __name__ == "__main__":
