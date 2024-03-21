4d5a33c6df70f99809f2d920c251e0d49d110c8e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-03-21 19:20:37 -0300

    Editing task file

---
+++ q
@@ -0,0 +1,24 @@
+[33m27ce6e3[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m Moving utils folder from app/ to src/
+[33m5645868[m Merge branch 'main' of https://github.com/rubensrabelo/python_projects
+[33m5999208[m Moving interface folder from app/ to src/
+[33m772dd14[m Creating __init__.py file in interface folder
+[33ma271272[m Moving database folder from app/ to src/
+[33mf29bfd0[m Creating __init__.py file in database folder
+[33m741666e[m Changing the files structures
+[33m7f7f104[m Editing task file
+[33m631cb1d[m Changing the utils file dictory from src to app
+[33m8a65d87[m Adding docstring in the Task Class
+[33m1ccc3f1[m Changing the import of the Validate Class int the Task and Task Manager Classes
+[33m7dc7644[m Removing the Validation.py file
+[33mabff24b[m Move Validate Classe from app/ to utils/
+[33m802de3a[m Editing Validate Class error alert
+[33mb3049d4[m Adding docstring in the Validate Class
+[33md11b641[m Editing the Task Manager Class
+[33m4185974[m Create the Validate Class method to check the index limit of a list
+[33m3eb8ec6[m Adding the task_manager methods
+[33m3c3adb4[m Editing a task class
+[33m97425c0[m Adding the task class methods
+[33m23d284f[m Editing the folder_structure.md file in docs
+[33mfe6657e[m Adding the validate class methods
+[33m4a815b3[m Add the README.md file
+[33ma03ba79[m Saving the To Do List file
--- to_do_list/root/src/app/task/task.py
+++ to_do_list/root/src/app/task/task.py
@@ -1,51 +1,30 @@
-import sys
-sys.path.append("/to_do_list/root/src/utils/")
 from datetime import datetime
-
-from utils.validate import Validate
-
 
 class Task():
     """
     Representa uma tarefa que possui uma data de vencimento, um nome descritivo e um status de conclusão.
 
     Atributos:
-        date (date): A data de vencimento da tarefa.
+        conclusion_date (datetime.date): A data de vencimento da tarefa.
         name (str): O nome descritivo da tarefa.
         status (int): O status de conclusão da tarefa, onde:
             - 0: Tarefa não concluída
             - 1: Tarefa concluída
+            - 2: Tarefa incompleta
     """
-
-    def __init__(self, date, name, status=0):
+    def __init__(self, date, name, status=0): 
         """
         Inicializa uma nova instância da classe Task.
 
         Args:
             date (str): A data da tarefa no formato 'dd/mm/yyyy'.
             name (str): O nome da tarefa.
-            status (int, optional): O status da tarefa. Padrão é 0.
-
-        Raises:
-            ValueError: Se a data estiver em formato inválido.
-            TypeError: Se o nome não for uma string.
-            ValueError: Se o nome estiver vazio.
-            TypeError: Se o status não for um inteiro.
-            ValueError: Se o status estiver fora do intervalo permitido.
+            status (int, optional): O status da tarefa. Padrão é 0 (tarefa não concluída).
         """
-        if Validate.validate_date(date):
-            raise ValueError("Error - Invalid date format, please use the format dd/mm/yyyy.")
-        
-        Validate.validate_name(name)
-        Validate.validate_status(status)
-        
-        self.date_str_conclusion = date
-
-        # Alert: I have to test if the user change correct the date
-
-        self.date_conclusion = datetime.strptime(date, "%d/%m/%Y").date()
+        self.conclusion_date = datetime.strptime(date, "%d/%m/%Y")
         self.name = name
         self.status = status
+        self.str_date = date
 
     def __str__(self) -> str:
         """
@@ -54,7 +33,7 @@ class Task():
         Returns:
             str: Uma string contendo a data, o nome e o status da tarefa.
         """
-        return f"Date: {self.date_str_conclusion}, Name: {self.name}, Status: {self.status}"
+        return f"Date: {self.str_date}, Name: {self.name}, Status: {self.status}"
 
     def __repr__(self) -> str:
         """
@@ -63,9 +42,4 @@ class Task():
         Returns:
             str: Uma string contendo a data, o nome e o status da tarefa.
         """
-        return f"Task(Date: {self.date_str_conclusion}, Name: {self.name}, Status: {self.status})"
-
-if __name__ == "__main__":
-    task = Task("01/01/2022", "tarefa 01")
-
-    
+        return f"Task(Date: {self.str_date}, Name: {self.name}, Status: {self.status})"
