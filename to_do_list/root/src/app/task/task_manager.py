bad30605af8a9d3e3b5e093430fa0e204593380f
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-03-21 19:29:24 -0300

    Editing task_manager file

--- to_do_list/root/src/app/task/task_manager.py
+++ to_do_list/root/src/app/task/task_manager.py
@@ -1,33 +1,60 @@
 from task import Task
-from utils.validate import Validate
-
 
 class Task_manager():
+    """
+    Classe para gerenciar uma lista de tarefas.
+
+    Métodos:
+        add_task(date, name, status=0): Adiciona uma nova tarefa à lista.
+        remove_task(index): Remove a tarefa na posição especificada da lista.
+        alter_status(status, index): Altera o status de uma tarefa na lista.
+    """
+
     def __init__(self):
+        """
+        Inicializa uma nova instância da classe Task_manager.
+        """
         self.task_list = []
 
     def add_task(self, date, name, status=0):
+        """
+        Adiciona uma nova tarefa à lista.
+
+        Args:
+            date (str): Data da tarefa no formato 'dd/mm/yyyy'.
+            name (str): Nome descritivo da tarefa.
+            status (int, optional): Status da tarefa. Padrão é 0.
+        """
         task = Task(date, name, status)
         self.task_list.append(task)
 
     def remove_task(self, index):
-        if not Validate.validate_index(self.task_list, index):
-            raise IndexError("Error - Invalid Index.")
+        """
+        Remove a tarefa na posição especificada da lista.
 
-        del self.task_list.pop(index)
+        Args:
+            index (int): Índice da tarefa a ser removida.
+        """
+        self.task_list.pop(index)
     
     def alter_status(self, status, index):
-        if not Validate.validate_index(self.task_list, index):
-            raise IndexError("Error - Invalid Index.")
+        """
+        Altera o status de uma tarefa na lista.
 
+        Args:
+            status (int): Novo status da tarefa.
+            index (int): Índice da tarefa cujo status será alterado.
+        """
         self.task_list[index].status = status
 
     def __str__(self):
+        """
+        Retorna uma representação em string da lista de tarefas.
+
+        Returns:
+            str: Uma string contendo todas as tarefas da lista, numeradas.
+        """
         if not self.task_list:
             return "No tasks found. The task list is currently empty."
         
-        return "\n".join(str(task) for task in self.task_list)
-
-
-if __name__ == "__main__":
-    ...
+        return "\n".join(f"{index}: {task}" for index, task in enumerate(self.task_list))
